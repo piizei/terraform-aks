3b326e552560a9eee22bf754c057f98ac85b40d6
Petteri Johansson <191493+piizei@users.noreply.github.com>
2022-02-04 08:13:41 +0000

    acr

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -55,3 +55,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
   ]
 
 }
+
+# AKS access to ACR
+resource "azurerm_role_assignment" "acrpull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
